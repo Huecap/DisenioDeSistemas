ba2d0a7f5ec626dad86d0da04ea5036f679b5f99
Huecap <huecap7@gmail.com>
2024-05-16 05:12:44 -0300

    Agregando herramienta con datos de prueba

---
+++ Entrega 1/Codigo/HR_datos_prueba.py
@@ -0,0 +1,82 @@
+provincias = [
+    "Buenos Aires",
+    "Catamarca",
+    "Chubut",
+    "Córdoba",
+    "Corrientes",
+    "Entre Ríos",
+    "Formosa",
+    "Jujuy",
+    "La Pampa",
+    "La Rioja",
+    "Mendoza",
+    "Misiones",
+    "Neuquén",
+    "Río Negro",
+    "San Juan",
+    "San Luis",
+    "Santa Cruz",
+    "Santa Fe",
+    "Santiago del Estero",
+    "Tierra del Fuego, Antártida e Islas del Atlántico Sur",
+    "Tucumán"
+]
+    
+regiones_vitivinicolas = {
+        "Buenos Aires": ["Costa Atlántica", "Valle de Uco"],
+        "Catamarca": ["Cafayate", "Hualfin"],
+        "Chubut": ["Valle del Río Chubut", "Valle Inferior del Río Chubut"],
+        "Córdoba": ["Sierras de Córdoba", "Valle Calamuchita"],
+        "Corrientes": ["Río Uruguay", "Litoral Norte"],
+        "Entre Ríos": ["Río Uruguay", "Delta del Paraná"],
+        "Formosa": [],  # No wine regions currently listed
+        "Jujuy": ["Quebrada de Humahuaca", "Valles Calileños"],
+        "La Pampa": [],  # No wine regions currently listed
+        "La Rioja": ["Chilecito", "Famatina"],
+        "Mendoza": ["Valle de Uco", "Valle Central"],
+        "Misiones": [],  # No wine regions currently listed
+        "Neuquén": ["Neuquén", "Alto Valle del Río Negro"],
+        "Río Negro": ["Alto Valle del Río Negro", "Valle Inferior del Río Negro"],
+        "San Juan": ["Valle de Tulum", "Valle de Pedernales"],
+        "San Luis": ["Valle de San Francisco", "Valle del Conlara"],
+        "Santa Cruz": [],  # No wine regions currently listed
+        "Santa Fe": ["Río Paraná", "Salto Grande"],
+        "Santiago del Estero": ["Quebrada de las Salinas", "Ribera del Dulce"],
+        "Tierra del Fuego, Antártida e Islas del Atlántico Sur": [],  # No wine regions currently listed
+        "Tucumán": ["Cafayate", "Tafí del Valle"]
+    }
+
+bodegas_creativas = [
+        "Viñedos del Alba",
+        "Cumbres Andinas",
+        "Sol Naciente",
+        "Valle Escondido",
+        "Manos Argentinas",
+        "Sabor Malbec",
+        "Corazón de Tierra",
+        "Frutos del Viento",
+        "Viñedos del Sur",
+        "Estrellas del Desierto",
+        "Leyendas del Vino",
+        "Alma Gaucha",
+        "Raíces Argentinas",
+        "Viñedos del Río",
+        "Horizontes Libres",
+        "Sueños de Uva",
+        "Tesoros del Terroir",
+        "Viñedos del Sol",
+        "Ecos del Viento"
+    ]
+
+uvas_argentinas = [
+    "Malbec",
+    "Bonarda",
+    "Cabernet Sauvignon",
+    "Chardonnay",
+    "Merlot",
+    "Syrah",
+    "Sauvignon Blanc",
+    "Torrontés",
+    "Pinot Noir",
+    "Semillón"
+]
